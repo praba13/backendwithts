7b59ba46aa17b0f32c2714f45f473e9fe561bdd4
Praba <prama1303@gmail.com>
2023-02-06 00:03:48 +0100

    validate env, and split server file for production

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,13 +1,11 @@
 import mongoose from 'mongoose';
-
+import 'dotenv/config';
 //mongoose.set('strictQuery', false);
+import env from '../utils/validateEnv';
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      process.env.MONGO_CONNECTION_STRING!,
-      {}
-    );
+    const conn = await mongoose.connect(env.MONGO_CONNECTION_STRING, {});
     console.log(`MongoDB Connected on ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,23 +1,9 @@
-import express from 'express';
-import 'dotenv/config';
-//import mongoose from 'mongoose';
 import { connectDB } from './config/db';
+import app from './app';
+import env from './utils/validateEnv';
 
-const app = express();
-const PORT = process.env.PORT || 6000;
+const PORT = env.PORT || 6000;
 
-app.get('/', (req, res) => {
-  res.send('HALLO');
-});
-
-/*
-mongoose
-  .connect(process.env.MONGO_CONNECTION_STRING!)
-  .then(() => {
-    console.log('Mongo DB Connected Sucessfully!!!');
-  })
-  .catch(console.error);
-*/
 app.listen(PORT, () => {
   connectDB();
   console.log(`Server Running Successfully on PORT ${PORT}!!!`);
