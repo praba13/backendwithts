c2a26d7e1faa8c64eacc7e05e373351bc16fec1e
Praba <prama1303@gmail.com>
2023-02-12 09:58:51 +0100

    delete a single note is ready

--- backend/src/routes/noteRoutes.ts
+++ backend/src/routes/noteRoutes.ts
@@ -7,5 +7,6 @@ router.get('/', NotesController.getNotes);
 router.get('/:noteId', NotesController.getNote);
 router.post('/', NotesController.createNote);
 router.patch('/:noteId', NotesController.updateNote);
+router.delete('/:noteId', NotesController.deleteNote);
 
 export default router;
