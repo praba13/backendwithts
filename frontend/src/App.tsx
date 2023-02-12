a997c1386d01564e739df4ec91eb611a7a4bf220
Praba <prama1303@gmail.com>
2023-02-12 17:10:16 +0100

    react and bootstrap are installed

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <div className='App'>
+      <Button variant='primary'>Primary</Button>
+    </div>
+  );
+}
+
+export default App;
