f0e696928d97a144bf65e04a8705d8a6cb3d501a
Praba <prama1303@gmail.com>
2023-02-13 09:59:03 +0100

    fetch data from backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
+import React, { useEffect, useState } from 'react';
+import { Note } from './models/note';
 
 function App() {
-  return (
-    <div className='App'>
-      <Button variant='primary'>Primary</Button>
-    </div>
-  );
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  useEffect(() => {
+    async function loadNotes() {
+      try {
+        const response = await fetch('/api/notes', {
+          method: 'GET'
+        });
+        const notes = await response.json();
+        setNotes(notes);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    loadNotes();
+  }, []);
+
+  return <div className='App'>{JSON.stringify(notes)}</div>;
 }
 
 export default App;
