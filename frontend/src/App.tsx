62b45b1928fef7796d3553e083161349beca7151
Praba <prama1303@gmail.com>
2023-02-14 21:14:19 +0100

    style the Note component with bootstrap und css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Note } from './models/note';
+import { Container, Row, Col } from 'react-bootstrap';
+import { Note as NoteModel } from './models/note';
+import Note from './components/Note';
+import styles from './styles/NotesPage.module.css';
 
 function App() {
-  const [notes, setNotes] = useState<Note[]>([]);
+  const [notes, setNotes] = useState<NoteModel[]>([]);
 
   useEffect(() => {
     async function loadNotes() {
@@ -19,7 +22,18 @@ function App() {
     loadNotes();
   }, []);
 
-  return <div className='App'>{JSON.stringify(notes)}</div>;
+  return (
+    <Container>
+      <Row xs={1} md={2} xl={3} className='g-4'>
+        {/* {JSON.stringify(notes)} */}
+        {notes.map((note) => (
+          <Col key={note._id}>
+            <Note note={note} className={styles.note} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
 }
 
 export default App;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './styles/global.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
