7afb9cd55d90cb6ac05af20359266d2fbda2f37b
Praba <prama1303@gmail.com>
2023-02-15 21:56:15 +0100

    create and send Note from frontend to backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import { Note as NoteModel } from './models/note';
 import Note from './components/Note';
+import AddNoteDialog from './components/AddNoteDialog';
 import styles from './styles/NotesPage.module.css';
+import styleUtils from './styles/utils.module.css';
+import * as NotesApi from './network/notes_api';
 
 function App() {
   const [notes, setNotes] = useState<NoteModel[]>([]);
+  const [showAddNoteDialog, setShowAddNoteDialog] = useState(false);
 
-  useEffect(() => {
+  /* useEffect(() => {
     async function loadNotes() {
       try {
         const response = await fetch('/api/notes', {
@@ -20,10 +24,28 @@ function App() {
       }
     }
     loadNotes();
+  }, []); */
+
+  useEffect(() => {
+    async function loadNotes() {
+      try {
+        const notes = await NotesApi.fetchNotes();
+        setNotes(notes);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    loadNotes();
   }, []);
 
   return (
     <Container>
+      <Button
+        className={`mb-4 ${styleUtils.blockCenter}`}
+        onClick={() => setShowAddNoteDialog(true)}
+      >
+        Add New Note
+      </Button>
       <Row xs={1} md={2} xl={3} className='g-4'>
         {/* {JSON.stringify(notes)} */}
         {notes.map((note) => (
@@ -32,6 +54,15 @@ function App() {
           </Col>
         ))}
       </Row>
+      {showAddNoteDialog && (
+        <AddNoteDialog
+          onDismiss={() => setShowAddNoteDialog(false)}
+          onNoteSaved={(newNote) => {
+            setNotes([...notes, newNote]);
+            setShowAddNoteDialog(false);
+          }}
+        />
+      )}
     </Container>
   );
 }
