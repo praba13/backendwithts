2869975eaf8c599f8e9a0aefff001676e727f7da
Praba <prama1303@gmail.com>
2023-02-20 22:52:01 +0100

    destroy with logout the session

--- backend/src/controllers/userControllers.ts
+++ backend/src/controllers/userControllers.ts
@@ -123,3 +123,13 @@ export const login: RequestHandler<
     next(error);
   }
 };
+
+export const logout: RequestHandler = (req, res, next) => {
+  req.session.destroy((error) => {
+    if (error) {
+      next(error);
+    } else {
+      res.sendStatus(200);
+    }
+  });
+};
--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -6,5 +6,5 @@ const router = express.Router();
 router.get('/', UserController.getAuthenticatedUser);
 router.post('/signup', UserController.signup);
 router.post('/login', UserController.login);
-
+router.post('/logout', UserController.logout);
 export default router;
